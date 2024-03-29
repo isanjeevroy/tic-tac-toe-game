03038d44a9aec1f1c3757c480a46979fe47340db
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-03-29 13:09:53 +0530

    style updated

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 let boxes = document.querySelectorAll(".box");
 
 let msg = document.querySelector("#msg");
+let turns = document.querySelector("#turns");
 
 
 let playerX = document.querySelector("#user1");
 let playerO = document.querySelector("#user2");
 let play = document.querySelector("#play");
 let users = document.querySelector(".users");
+let usersIdnt = document.querySelector(".users-identity");
 
 const newGame = document.querySelector("#new-game");
 const resetGame = document.querySelector("#reset-game");
 
 let turnO=true;
+
+// Display user identity
+    
 let winner =[
     [0,1,2],
     [3,4,5],
@@ -35,6 +40,7 @@ const showWinner =(pos1)=>{
     for(let box of boxes){
         box.disabled=true;
     }
+    turns.style.display="none";
 }
 
 // check the winner
@@ -56,14 +62,20 @@ const checkWinner=()=>{
 // logic here
 
 const playGame = ()=>{
+    usersIdnt.innerText=`${playerX.value} = X , ${playerO.value} = O`;
+
     boxes.forEach((box) => {
         box.addEventListener("click",() => {
+            turns.style.display="block";
             if(playerO.value!="" && playerX.value!=""){
-                if(turnO){
+                if(turnO){ 
                     box.innerText ="O";
+                    turns.innerText=`It's ${playerX.value} turns.`;
                     turnO=false;
+                    
                 }else{
                     box.innerText ="X";
+                    turns.innerText=`It's ${playerO.value} turns.`;
                     turnO=true;
                 }
                box.disabled=true;  
@@ -77,12 +89,23 @@ const playGame = ()=>{
 
 const resetGameFun = () =>{
     users.style.display="none";
-    turnO=true;
+    turns.style.display="block";
+
     boxes.forEach((box)=>{
         box.disabled = false;
         box.innerText="";
         msg.innerText="";
     });
+      
+    if(pos1==="X"){
+        turnO=false;
+        turns.innerText=`It's ${playerX.value} turns.`;
+        
+    }else{
+        turnO=true;
+        turns.innerText=`It's ${playerO.value} turns.`;
+    }
+    
 }
 
 resetGame.addEventListener("click",resetGameFun);
@@ -90,7 +113,7 @@ resetGame.addEventListener("click",resetGameFun);
 play.addEventListener("click",()=>{
     if(playerO.value!="" && playerX.value!=""){
         users.style.display="none";
-
+        turns.style.display="block";
         playGame();
     }
     else{
